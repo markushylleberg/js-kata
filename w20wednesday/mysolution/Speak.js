8d75742e8a243c0dd81f98a480587099ebf0637a
Markus Hylleberg <markushylleberg@gmail.com>
2018-05-16 21:37:12 +0200

    hej

---
+++ w20wednesday/mysolution/Speak.js
@@ -0,0 +1,51 @@
+"use strict";
+
+let liste = document.querySelector("ul");
+
+class Speak {
+    constructor(){
+        this.synth = window.speechSynthesis;
+        this.pitch = 1;
+        this.rate = 0.9;
+        this.voices=[];
+        this.voice=0;
+        this.populateVoiceList();
+        return this;
+    }
+    populateVoiceList() {
+        
+        window.speechSynthesis.onvoiceschanged = function() {
+          this.voices = this.synth.getVoices();
+        }.bind(this);
+        return this;
+    }
+    getVoices(output=false){
+        if(!output){
+            return this.voices;    
+        }
+        console.table(this.voices)
+    }
+    setVoice(index){
+        this.voice=index;
+        return this;
+    }
+    setPitch(pitch){
+        this.pitch=pitch;
+        return this;
+    }
+    setRate(rate){
+        this.rate=rate;
+        return this;
+    }
+    speak(what){
+        let utterThis = new SpeechSynthesisUtterance(what);
+        utterThis.voice = this.voices[this.voice];
+        utterThis.pitch = this.pitch;
+        utterThis.rate = this.rate;
+        this.synth.speak(utterThis);
+        return this;
+    }
+    
+    
+}
+
